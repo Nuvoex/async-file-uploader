e07a84f79903388bb7a059bd7db57a22154cee94
Dilip <dilipvrk@gmail.com>
2017-01-05 15:49:24 +0530

    Removed unused resources

--- fileuploader/src/androidTest/java/com/nuvoex/fileuploader/ExampleInstrumentedTest.java
+++ fileuploader/src/androidTest/java/com/nuvoex/fileuploader/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.nuvoex.imageuploader", appContext.getPackageName());
+        assertEquals("com.nuvoex.fileuploader", appContext.getPackageName());
     }
 }
