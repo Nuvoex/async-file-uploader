5ba6c41d86ed7305b3074ef28dd048e4165fee65
Dilip <dilipvrk@gmail.com>
2017-01-05 17:25:09 +0530

    Removed http logging

--- fileuploader/src/main/java/com/nuvoex/fileuploader/network/ApiManager.java
+++ fileuploader/src/main/java/com/nuvoex/fileuploader/network/ApiManager.java
@@ -1,13 +1,10 @@
 package com.nuvoex.fileuploader.network;
 
-import android.util.Log;
-
 import com.nuvoex.fileuploader.utils.Consts;
 
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 
 /**
@@ -21,17 +18,9 @@ public class ApiManager {
 
     private synchronized static Retrofit getClient() {
         if (retrofit == null) {
-            HttpLoggingInterceptor logging = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
-                @Override
-                public void log(String message) {
-                    Log.d(Consts.TAG, message);
-                }
-            });
-            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
             OkHttpClient client = new OkHttpClient.Builder()
                     .readTimeout(Consts.Configs.TIMEOUT, TimeUnit.SECONDS)
                     .connectTimeout(Consts.Configs.TIMEOUT, TimeUnit.SECONDS)
-                    .addInterceptor(logging)
                     .build();
             retrofit = new Retrofit.Builder()
                     .baseUrl(Consts.Urls.BASE_URL)
